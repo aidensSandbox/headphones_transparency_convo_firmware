aca2da68748372b82643a03b37a683e258f2d3bb
Ahmed Ibrahim <ahmedibrahim112@gmail.com>
2016-05-09 22:44:57 -0400

    First release cut for Audesis Prototype. This version has the following functionality: 1) Mic w music mix 2) Music and mic curation for clarity
    Needs to be addded:
    1) Auto volume limit
    2) Convo enhance ( mute mics when user speaks)
    3) Ramp transition between modes

--- AudesisPx - MVP/APx.0.2/APx.0.2.ino
+++ AudesisPx - MVP/APx.0.2/APx.0.2.ino
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////
 //AUDESIS.PX CODE FOR MVP/////////////////////////////////
-//REVISION: 3, switched to GitLAB starting version 3//////
+//REVISION: 2.5, switched to GitLAB starting version 2.5//////
 /////////////////////////////////////////////////////////
 //FUNCTION:
 //1) Mics are functional but finniky
@@ -44,96 +44,99 @@ AudioControlSGTL5000     sgtl5000_2;     //xy=492,44
 // GUItool: end automatically generated code
 
 const int myInput = AUDIO_INPUT_LINEIN;
-const int buttonPin = 2;
+const int buttonPin = 26;
 
 int buttonState;
-int lastButtonState  =LOW;
+int lastButtonState  = LOW;
 int news = 1; //variable used to ensure that only new status updates are sent across serial
 int mode = -1;//switch between audesis modes by changing constant below:
-float volx = 1; //dac volume max
 long lastDebounceTime = 0;
 long debounceDelay = 50;
+float maxmusicvol=0.75;
 elapsedMillis currentTime; //current clock time elapsed since execution (reboot)
+//float currentTime = millis();
 
-void rampUp(AudioControlSGTL5000 & audioShield){
+void rampUp(AudioControlSGTL5000 & audioShield) {
 
   //define variables
-  float rampTime = 5000; //volume initial ramp time
+  float rampTime = 3000; //volume initial ramp time
   float delayTime = 50; //ms
-  int x=0;
+  int x = 0;
   unsigned int startTime;
-  startTime=currentTime;
-    
+  startTime = currentTime;
+
   //rampStep = 1 / rampTime;
   //delayTime = rampTime / rampStep;
   Serial.println("start");
   while (currentTime < startTime + rampTime) {
-    float elapsedTime = currentTime-startTime;
-    float vol = elapsedTime/rampTime;
-    audioShield.volume(vol*0.75);
-    Serial.println(vol,4);
+    float elapsedTime = currentTime - startTime;
+    float vol = elapsedTime / rampTime;
+    audioShield.volume(vol * maxmusicvol);
+    Serial.println(vol, 4);
     delay(delayTime);
-    }
-   audioShield.dacVolume(1); //just in case I skip an iteration
+  }
+  audioShield.volume(maxmusicvol); //just in case I skip an iteration
 }
 
 void setup() {
   //Begin serial
-   Serial.begin(9600);
-   
+  Serial.begin(9600);
+
   //Setup button
-    pinMode(buttonPin,INPUT_PULLUP);
-  
+  pinMode(buttonPin, INPUT_PULLUP);
+
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
   AudioMemory(50);
 
   // Enable the first audio shield (Music), select input, and enable output
   sgtl5000_1.setAddress(LOW);
-  //sgtl5000_1.dacVolume(0);
   sgtl5000_1.enable();
-  sgtl5000_1.muteHeadphone();
   sgtl5000_1.volume(0); //0.75
   sgtl5000_1.inputSelect(myInput);
   sgtl5000_1.audioPreProcessorEnable();
-  
+
 
   // Enable the second audio shield (Mics), select input, and enable output
-  //sgtl5000_2.setAddress(HIGH);
-  //sgtl5000_2.enable();
-  //sgtl5000_2.inputSelect(myInput);
-  //sgtl5000_2.volume(0.75);
-  //sgtl5000_2.audioPreProcessorEnable();
+  sgtl5000_2.setAddress(HIGH);
+  sgtl5000_2.enable();
+  sgtl5000_2.volume(0.75);
+  sgtl5000_2.inputSelect(myInput);
+  sgtl5000_2.audioPreProcessorEnable();
 
-//xxxxxxxxxxxxxxxxxxxthis will change once new filters are added in Rev3
-//xxxxxxxxxxNotes: mixer 1 = left ear, mixer 2 = right ear
-//xxxxxxxxxxxx mixer1/2 channel 0 is music, channel 1 is Mic
+  //xxxxxxxxxxxxxxxxxxxthis will change once new filters are added in Rev3
+  //xxxxxxxxxxNotes: mixer 1 = left ear, mixer 2 = right ear
+  //xxxxxxxxxxxx mixer1/2 channel 0 is music, channel 1 is Mic
 
-//Initial boot on Deep Focus mode
-//music mix
-mixer1.gain(0,0.75);
-mixer2.gain(0,0.75);
-//mic mix
-mixer1.gain(1,0);
-mixer2.gain(1,0);
+  //Initial boot on Deep Focus mode
+  //EQ for Music
+  sgtl5000_1.eqSelect(3);
+  sgtl5000_1.eqBands(0,-0.4,-0.5,-0.4,-0.2); //115Hz, 330Hz, 990Hz, 3kHz, 8.8kHz
+  //sgtl5000_1.eqBands(0,0,0,0,0); //115Hz, 330Hz, 990Hz, 3kHz, 8.8kHz
+  
+  //music mix
+  mixer1.gain(0, 0.75);
+  mixer2.gain(0, 0.75);
+  //mic mix
+  mixer1.gain(1, 0);
+  mixer2.gain(1, 0);
 
 
-Serial.println("Start Ramping Up");
-sgtl5000_1.dacVolumeRampDisable();
-rampUp(sgtl5000_1);
-Serial.println("done ramping");
-//sgtl5000_1.muteHeadphone();
+  Serial.println("Start Ramping Up");
+  //sgtl5000_1.dacVolumeRampDisable();
+  rampUp(sgtl5000_1);
+  Serial.println("done ramping");
 }
 
 void loop() {
-///////first ramp in volume at deep focus mode////
-//you want this function to freeze everything below anyway so using
-//delay() is fine
-//sgtl5000_1.dacVolumeRampDisable()
+  ///////first ramp in volume at deep focus mode////
+  //you want this function to freeze everything below anyway so using
+  //delay() is fine
+  //sgtl5000_1.dacVolumeRampDisable()
 
-int reading = digitalRead(buttonPin);
+  int reading = digitalRead(buttonPin);
 
-//initially ramp volume up to avoid any mic feedback or so
+  //initially ramp volume up to avoid any mic feedback or so
 
   // If the switch changed, due to noise or pressing:
   if (reading != lastButtonState) {
@@ -141,7 +144,7 @@ int reading = digitalRead(buttonPin);
     lastDebounceTime = millis();
   }
 
- if ((millis() - lastDebounceTime) > debounceDelay) {
+  if ((millis() - lastDebounceTime) > debounceDelay) {
     // whatever the reading is at, it's been there for longer
     // than the debounce delay, so take it as the actual current state:
 
@@ -149,84 +152,106 @@ int reading = digitalRead(buttonPin);
     if (reading != buttonState) {
       buttonState = reading;
 
-      // only toggle the LED if the new button state is HIGH
+      // only toggle the mode if the new button state is HIGH
       if (buttonState == HIGH) {
-          mode=mode+1;
-          Serial.print("Button released, mode: ");
-          Serial.println(mode);
-          Serial.print("RampStep = ");
-          //Serial.println(rampStep,5);
-          news=1;
+        mode = mode + 1;
+        Serial.print("Button released, mode: ");
+        Serial.println(mode);
+        news = 1;
       }
     }
   }
 
 
-if (mode == 0 && news == 1) {
-//Deep focus mode; mode 0
-  //if (news==1) {
-Serial.println("Deep focus activated");
-news=0;
-  //}
-//music mix
-mixer1.gain(0,0.75);
-mixer2.gain(0,0.75);
-//mic mix
-mixer1.gain(1,0); //might shut this off to save battery (using disable() or mutelinein())
-mixer2.gain(1,0);
-
-sgtl5000_1.autoVolumeEnable();
-sgtl5000_1.autoVolumeControl(0,0,0,-18,10,15);
-}
-
-if (mode ==1 && news == 1) {
-//conversation; mode1
-//notes: get mics to percieve conversation range with EQ, apply inv.vocal boost on music
-
-Serial.println("Conversation/temp DF mode activated");
-sgtl5000_1.autoVolumeDisable();
-news=0;
-
-/*
-//music mix
-mixer1.gain(0,0.05);
-mixer2.gain(0,0.05);
-//mic mix
-mixer1.gain(1,0.065);
-mixer2.gain(1,0.065);
-*/
-
-//music mix
-mixer1.gain(0,0.75);
-mixer2.gain(0,0.75);
-//mic mix
-mixer1.gain(1,0);
-mixer2.gain(1,0);
-
-}
-
-if (mode ==2 && news == 1) {
-//Awareness mode; mode 2
-//notes: get mics to percieve what's going on around, no EQ on music,
-//add EQ on mic for most natural surround sound
+  if (mode == 0 && news == 1) {
     
-Serial.println("Awareness mode activated");
-news=0;
+    //EQ for Music
+    sgtl5000_1.eqSelect(3);
+    sgtl5000_1.eqBands(0,-0.4,-0.5,-0.4,-0.2); //115Hz, 330Hz, 990Hz, 3kHz, 8.8kHz
+    //sgtl5000_1.eqBands(0,0,0,0,0); //115Hz, 330Hz, 990Hz, 3kHz, 8.8kHz
 
-//music mix
-mixer1.gain(0,0.2);
-mixer2.gain(0,0.2);
-//mic mix
-mixer1.gain(1,0.05);
-mixer2.gain(1,0.05);  
-}
-//Serial.println("working");
-//delay(100);
-if (mode>2) {
-mode=0;
-}
+    //EQ for Mic
+    sgtl5000_2.eqSelect(3);
+    sgtl5000_2.eqBands(0,0,0,0,0);
+    
+    //Deep focus mode; mode 0
+    //if (news==1) {
+    Serial.println("Deep focus activated");
+    news = 0;
+    //}
+    //music mix
+    mixer1.gain(0, 0.75);
+    mixer2.gain(0, 0.75);
+    //mic mix
+    mixer1.gain(1, 0); //might shut this off to save battery (using disable() or mutelinein())
+    mixer2.gain(1, 0);
+  }
 
-// save the reading.  Next time through the loop,
-// it'll be the lastButtonState:
-lastButtonState = reading;
+  if (mode == 1 && news == 1) {
+    //conversation; mode1
+    //notes: get mics to percieve conversation range with EQ, apply inv.vocal boost on music
+
+      //EQ for Music
+      sgtl5000_1.eqSelect(3);
+      sgtl5000_1.eqBands(1,-1,-1,-1,0.7); //115Hz, 330Hz, 990Hz, 3kHz, 8.8kHz
+  
+      //EQ for Mic
+      sgtl5000_2.eqSelect(3);
+      sgtl5000_2.eqBands(-0.2,0,0,0,-0.2);
+     
+      Serial.println("Conversation activated");
+      news = 0;
+/*
+    //music mix
+    mixer1.gain(0, 0.4);
+    mixer2.gain(0, 0.4);
+    //mic mix
+    mixer1.gain(1, 0); //might shut this off to save battery (using disable() or mutelinein())
+    mixer2.gain(1, 0);
+
+  }
+ */    
+      //music mix
+      mixer1.gain(0,0.01);
+      mixer2.gain(0,0.01);
+      //mic mix
+      mixer1.gain(1,0.065);
+      mixer2.gain(1,0.065);
+
+  }
+
+  if (mode == 2 && news == 1) {
+    //Awareness mode; mode 2
+    //notes: get mics to percieve what's going on around, no EQ on music,
+    //add EQ on mic for most natural surround sound
+
+    //EQ for Music
+    sgtl5000_1.eqSelect(3);
+    sgtl5000_1.eqBands(0,-0.4,-0.5,-0.4,-0.2); //115Hz, 330Hz, 990Hz, 3kHz, 8.8kHz
+    //sgtl5000_1.eqBands(0,0,0,0,0); //115Hz, 330Hz, 990Hz, 3kHz, 8.8kHz
+
+    //EQ for Mic
+    sgtl5000_2.eqSelect(3);
+    sgtl5000_2.eqBands(-0.2,0,0,0,-0.2);
+
+    Serial.println("Awareness mode activated");
+    news = 0;
+
+    //music mix
+    mixer1.gain(0, 0.1);
+    mixer2.gain(0, 0.1);
+    //mic mix
+    mixer1.gain(1, 0.060);
+    mixer2.gain(1, 0.060);
+  }
+
+  //Serial.println("working");
+  //delay(100);
+  if (mode > 2) {
+    mode = 0;
+  }
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
 }
